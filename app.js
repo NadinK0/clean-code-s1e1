83aab81f0c93ef2c46c0f267320d4c2828463fe8
Nadezhda Klinskaya <npantelyeva@gmail.com>
2024-12-17 01:27:02 +0300

    refactor(extended-2.1): updated JS file, using new classnames

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ var createNewTaskElement=function(taskString){
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="added-tasks__item";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="added-tasks__edit";
